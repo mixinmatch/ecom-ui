b0bc2e6841769591a1b04ba3d6db89f346e30232
wayne <chevreuilqwerty@gmail.com>
2026-06-11 18:47:49 -0400

    remove

--- environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  CATALOG_API: 'http://localhost:8080',
-  INVENTORY_API: 'http://localhost:5053',
-  APPT_API: 'http://localhost:5062',
-  ORDER_API: 'http://localhost:5274'
-};
