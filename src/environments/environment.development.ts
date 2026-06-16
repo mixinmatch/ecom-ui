dc6f170c0ec81007fc05f5ce1ec43ccee2a1025a
wayne <chevreuilqwerty@gmail.com>
2026-06-16 14:54:33 -0400

    Empty url. Use relative path

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,11 @@
 export const environment = {
     production: false,
-    CATALOG_URL: "http://localhost:51230",
-    INV_URL: "http://localhost:7075",
-    ORDER_URL: "http://localhost:7286",
-    APPT_URL: "http://localhost:7265",
+    CATALOG_URL: "",
+    INV_URL: "",
+    ORDER_URL: "",
+    APPT_URL: "",
+    // CATALOG_URL: "http://localhost:51230",
+    // INV_URL: "http://localhost:7075",
+    // ORDER_URL: "http://localhost:7286",
+    // APPT_URL: "http://localhost:7265",
 };
